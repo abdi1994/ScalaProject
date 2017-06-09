6aea828a566d2d3d835d83d76b50e403b714bc13
abdi1994 <abdi1994@hotmail.com>
2017-06-09 08:10:19 +0000

    First Commit (Day 5)

--- src/main/scala/Garage/Person.scala
+++ src/main/scala/Garage/Person.scala
@@ -5,15 +5,26 @@ package Garage
   */
 class Person {
 
-  private var name = ""
+  private var name : String = ""
+  private var age : Int = null
 
 
   //Setter
 
-
-
+  def Person (str1 : String, int1 : Int): Unit = {
+    this.name = str1
+    this.age = int1
+  }
 
   //Getter
+
+  def getName : String = {
+    name
+  }
+
+  def getAge : String = {
+    name
+  }
 
 
 }
