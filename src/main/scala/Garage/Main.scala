c7f3b023f9c2b80b9589d08e344dba8588d6b9e0
abdimohammed <abdi1994@hotmail.com>
2017-06-17 12:05:32 +0100

    /

--- src/main/scala/Garage/Main.scala
+++ src/main/scala/Garage/Main.scala
@@ -7,7 +7,7 @@ class Main {
 
   def main(args: Array[String]): Unit = {
 
-    val n = new Customer("Adam", "Jones", 20,"test", "test", 20, List(new Car("")))
+    val n = new Customer("Test", "Test", 20,"test", "test", 20, List(new Car("")))
 
 
 
